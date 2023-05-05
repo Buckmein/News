a96e417d8774bbc1f7002de1958678056f81389d
Buckmein <dodin.dima@gmail.com>
2023-05-05 12:29:45 +0300

    D6

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -1,12 +1,12 @@
 from django import template
 register = template.Library()
-swear_word = ['пидор', "пидр", "пидрила", "капибара"]
+swear_word = ["колибри", "птица", "птиц", "птицы"]
 
 
 @register.filter()
 def cenz(text):
     for i in text.split():
-        if i in swear_word:
+        if i.lower() in swear_word:
             text = text.replace(i, f'***{i[0]}***')
 
     return text
